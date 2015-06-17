263c0753386c797584d2f3fc21255211ed64cd86
Patryk Strach <P.Strachu@gmail.com>
2015-06-17 10:53:56 +0200

    Remove Clean.bat

--- Clean.bat
+++
@@ -1,5 +0,0 @@
-FOR /F "tokens=*" %%G IN ('dir /B /AD /S bin') DO rmdir /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('dir /B /AD /S obj') DO rmdir /S /Q "%%G"
-
-FOR /F "tokens=*" %%G IN ('dir /B /AD /S bin') DO rmdir /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('dir /B /AD /S obj') DO rmdir /S /Q "%%G"
